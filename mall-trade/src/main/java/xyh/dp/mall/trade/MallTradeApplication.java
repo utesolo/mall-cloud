2ca56487ac0ad59c5053a9e7939fc4ee7a5a711a
嘉然今天吃什么 <2821260061@qq.com>
2025-12-13 22:22:58 +0800

    feat: 添加feign来跨模块调用

--- mall-trade/src/main/java/xyh/dp/mall/trade/MallTradeApplication.java
+++ mall-trade/src/main/java/xyh/dp/mall/trade/MallTradeApplication.java
@@ -3,6 +3,7 @@ package xyh.dp.mall.trade;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "xyh.dp.mall.trade.feign")
 @ComponentScan(basePackages = {"xyh.dp.mall.trade", "xyh.dp.mall.common"})
 public class MallTradeApplication {
 
